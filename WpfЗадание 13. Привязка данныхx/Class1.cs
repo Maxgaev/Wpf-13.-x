434df2aebc113346e57beb29f9480cc75c419f89
Максим Алехин <Max-crf@mail.ru>
2022-02-26 17:33:16 +0700

    Добавьте файлы проекта.

---
+++ WpfЗадание 13. Привязка данныхx/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfЗадание_13.Привязка_данныхx
+{
+    internal class Class1
+    {
+        public static RoutedUICommand Exit { get; set; }
+        static Class1()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.F4, ModifierKeys.Alt, "Alt+F4"));
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(Class1), inputs);
+        }
+
+    }
+}
